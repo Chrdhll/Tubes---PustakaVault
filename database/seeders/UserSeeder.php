58a3edddc3da62d93624540d7921223d74294227
Fadhil <m.fadil3434@gmail.com>
2025-07-02 01:59:10 +0700

    README

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,11 +13,18 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-         User::create([
+        User::create([
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
-            'password' => Hash::make('12345'), 
+            'password' => Hash::make('12345'),
             'role' => 'admin',
+        ]);
+
+        User::create([
+            'name' => 'Member',
+            'email' => 'member@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'member',
         ]);
 
         User::factory()->count(10)->create();
